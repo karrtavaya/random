cf9e5ffe2a1ff9271dfae3a3fb022d224772a4a8
asus <asus@DESKTOP-G8GJDTP>
2022-09-19 10:22:18 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp4
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[25];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(25);
+                Console.WriteLine(array[i]);
+            }
+            int sum = 0;
+            for (int c = 0; c<5; c++)
+            {
+                sum = array[c] + sum;
+
+            }
+            Console.WriteLine("сумма = {0}", sum);
+        }
+    }
+}
+        
